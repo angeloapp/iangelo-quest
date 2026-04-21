9bfadb9e9e3c7ea5bac7334a638331acdadae59c
angeloapp <angeloapp@users.noreply.github.com>
2026-04-21 11:08:51 -0700

    Replace v1.4 file with updated imgur images for maps and creatures

---
+++ v1.4.js
@@ -0,0 +1,31 @@
+// Updated v1.4
+
+const maps = {
+    stone: 'https://i.imgur.com/7E01HkA.png',
+    cave: 'https://i.imgur.com/7D5oYpH.png',
+    ice: 'https://i.imgur.com/rdOihAJ.png',
+    grass: 'https://i.imgur.com/kgO2wig.png',
+    lava: 'https://i.imgur.com/gXOzXEt.png',
+    swamp: 'https://i.imgur.com/EwI8sEx.png',
+    earth: 'https://i.imgur.com/lTyQpIG.png',
+    temple: 'https://i.imgur.com/BI4enIt.png'
+};
+
+const creatures = {
+    archer: 'https://i.imgur.com/GZxAHZx.png',
+    shaman: 'https://i.imgur.com/1xs0yTp.png',
+    ancestralDragon: 'https://i.imgur.com/E4h3EZR.png',
+    drake: 'https://i.imgur.com/ZilkTqx.png',
+    skeleton: 'https://i.imgur.com/Ms8SPNL.png',
+    gnoll: 'https://i.imgur.com/IcqAtEk.png',
+    goblin: 'https://i.imgur.com/ZrBfTAl.png',
+    warrior: 'https://i.imgur.com/aRQGe2O.png',
+    lichKing: 'https://i.imgur.com/ThPFURk.png',
+    alphaWolf: 'https://i.imgur.com/HhgemOw.png',
+    mage: 'https://i.imgur.com/6MlxHBs.png',
+    redMetin: 'https://i.imgur.com/bPzRR0M.png',
+    necromancer: 'https://i.imgur.com/Rtm9R20.png'
+};
+
+// SVG menus for the UI remain unchanged
+// ... (existing SVG menu code)
